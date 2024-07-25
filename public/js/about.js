929d0a3605552bbcdb83f9dd55d0f7981eae3e1d
code-with-santanu <santanupal003@gmail.com>
2024-07-26 01:48:53 +0530

    animation added

--- public/js/about.js
+++ public/js/about.js
@@ -1,4 +1,83 @@
-function setVisibility(id, visibility) {
-  document.getElementById(id).style.display = visibility;
-  document.getElementById(id).style.transitionDuration = 2;
+// function setVisibility(id, visibility) {
+//   document.getElementById(id).style.display = visibility;
+//   document.getElementById(id).style.transitionDuration = 2;
+// }
+
+// Func to open the read-more content
+function viewRM() {
+  abtl.restart();
+  console.log("abtl played");
 }
+
+// Func to close the read-more content
+function hideRM() {
+  abtl.reverse();
+  console.log("abtl reversed");
+}
+/* ------------------GSAP ANIMATION------------------ */
+gsap.from(".about-content p span", {
+  y: 10,
+  opacity: 0,
+  stagger: 0.8,
+  scrollTrigger: {
+    trigger: ".about-content p",
+    scroller: "body",
+    // markers: true,  // dev tools
+    start: "top 56%",
+    end: "top 28%",
+    scrub: 4,
+  },
+});
+
+// read-more animation starts here
+const abtl = gsap.timeline();
+abtl.pause();
+
+abtl.to(".read-more", {
+  display: "flex",
+  zIndex: 10001,
+  duration: 0.5,
+  opacity: 1,
+});
+
+abtl.from(".read-more #block1 #sub1", {
+  x: -200,
+  opacity: 0,
+  delay: 0.2,
+  duration: 0.2,
+});
+
+abtl.from(".read-more #block1 #sub2", {
+  y: 100,
+  opacity: 0,
+  delay: 0.1,
+  duration: 0.3,
+});
+
+abtl.from(".read-more #block2 #sub1", {
+  x: 200,
+  opacity: 0,
+  delay: 0.2,
+  duration: 0.2,
+});
+
+abtl.from(".read-more #block2 #sub2", {
+  y: 200,
+  opacity: 0,
+  delay: 0.1,
+  duration: 0.3,
+});
+
+abtl.from(".read-more #block3 #sub1", {
+  x: -200,
+  opacity: 0,
+  delay: 0.2,
+  duration: 0.2,
+});
+
+abtl.from(".read-more #block3 #sub2", {
+  y: 200,
+  opacity: 0,
+  delay: 0.1,
+  duration: 0.3,
+});
